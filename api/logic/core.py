5955156c9cb247f1242d46d76d44ce06f576d1f3
Hans Kristanto <kristanto.hs@gmail.com>
2020-04-10 19:53:40 +0800

    Fix tabbing issue

--- api/logic/core.py
+++ api/logic/core.py
@@ -4,44 +4,44 @@ from .db import read_food, add_food
 app = Flask(__name__)
 
 DEFAULT_RESPONSE_DATA = {
-	'ok': False,
-	'data': []
+    'ok': False,
+    'data': []
 }
 
 @app.route('/')
 @app.route('/index')
 def index():
-	r = DEFAULT_RESPONSE_DATA
-	r['data'] = {'message': 'Hello! Welcome to crudapp'}
-	r['ok'] = True
-	return jsonify(r)
+    r = DEFAULT_RESPONSE_DATA
+    r['data'] = {'message': 'Hello! Welcome to crudapp'}
+    r['ok'] = True
+    return jsonify(r)
 
 
 @app.route('/api/food', methods=['GET', 'POST'])
 def get_foods():
-	method = request.method
-	r = DEFAULT_RESPONSE_DATA
+    method = request.method
+    r = DEFAULT_RESPONSE_DATA
 
-	if method == 'GET':
-		sqlstr = 'SELECT * FROM foods'
-		id = request.args.get('id')
+    if method == 'GET':
+        sqlstr = 'SELECT * FROM foods'
+        id = request.args.get('id')
 
-		try:
-			r['data'] = read_food(id)
-		except Exception as e:
-			r['data'] = {'message': str(e)}
+        try:
+            r['data'] = read_food(id)
+        except Exception as e:
+            r['data'] = {'message': str(e)}
 
-	elif method == 'POST':
-		body = request.json
+    elif method == 'POST':
+        body = request.json
 
-		if body:
-			try:
-				add_food(body.get('food'))
-				r['ok'] = True
-				r['data'] = {'message': 'OK'}
-			except Exception as e:
-				r['data'] = {'message': str(e)}
-		else:
-			r['data'] = {'message': 'POST body must be provided'}
+        if body:
+            try:
+                add_food(body.get('food'))
+                r['ok'] = True
+                r['data'] = {'message': 'OK'}
+            except Exception as e:
+                r['data'] = {'message': str(e)}
+        else:
+            r['data'] = {'message': 'POST body must be provided'}
 
-	return jsonify(r)
+    return jsonify(r)
